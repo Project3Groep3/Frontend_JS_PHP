88aab5c1d2629fbacaaad47a591f953c7cb082a8
wesleyvano <tomawe@hotmail.nl>
2016-06-20 18:23:30 +0200

    Meer Structuur

--- included/header_main.php
+++ included/header_main.php
@@ -4,6 +4,7 @@ echo <<<HEADER
     <meta charset="utf-8" />
     <title>$name</title>
     <link rel="stylesheet" href="_js/jquery-ui/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/Main.css" type="text/css">
     <script src="_js/jquery-ui/external/jquery/jquery.js"></script>
     <script src="_js/jquery-ui/jquery-ui.min.js"></script>
     <script src="scripts/Main.js"></script>
--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <?php
+    // De naam van het project
     $name = 'Test';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <?php include 'included/header.php' ?>
+    <?php include 'included/header_main.php' ?>
     <body>
-        
+        <div id="title">
+            <h2>Evenementen</h2>
+        </div>
+        <div id="events">
+            <table>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>9</td>
+                    <td>10</td>
+                </tr>
+            </table>
+        </div>
     </body>
 </html>
